79937ed2d641599ceb863e3566b80e13cd3783ff
roy-van-dijk <roy.van.dijk@outlook.com>
2017-01-31 20:36:05 +0100

    Started working on new game mode

---
+++ js/municipalityFunctions.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$('.input').keyup(function(){
+
+		var input = makeSortString($('.input').val()).toLowerCase().trim();
+		var counter = 0;
+
+		$('.list ul li').each(function(){
+			if ($(this).attr('id') == colourCountry(input)){
+				if (!$(this).hasClass('correct')){
+					$(this).removeClass('incorrect');
+					$(this).addClass('correct');
+					counter = (parseInt($('.score').text()) + 1);
+					var test = colourCountry(input);
+					$('path[gem="'+test+'"]').css('fill', '#64f4a0');
+					$('circle[gem="'+test+'"]').css('fill', '#64f4a0');
+					$('.score').text(counter);
+					$('.input').val('');
+				}
+			}
+		});
+	});
+
+});
